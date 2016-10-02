37ee0119fd2c6e4d3e58bb14e1a2794d952c012c
Topbit Du <topbit.du@gmail.com>
2016-10-02 08:35:13 +0800

    1, Improve the Model Extension concern to support the Exact Column specification.

--- app/models/unidom/common/concerns/model_extension.rb
+++ app/models/unidom/common/concerns/model_extension.rb
@@ -178,8 +178,25 @@ module Unidom::Common::Concerns::ModelExtension
       end
     end
 
+    def exact_column(*names)
+      names.each do |name|
+        name = name.to_s
+        instance_eval do
+          scope :"#{name}_is", ->(value) { where "#{name}_exact_signature" => exact_signature(self, name, value) }
+          before_save do
+            send "#{name}_exact_signature=", self.class.exact_signature(self.class, name, send(name))
+          end
+        end
+      end
+    end
+
     def assert_present!(name, value)
       raise ArgumentError.new("The #{name} argument is required.") if value.blank?
+    end
+
+    def exact_signature(klass, name, value)
+      text = "#{Rails.application.secrets[:secret_key_base]}@#{Rails.root}/#{klass.table_name}##{name}=#{value}"
+      "#{Digest::MD5.digest(text)}#{Digest::SHA512.digest(text)}"
     end
 
   end
