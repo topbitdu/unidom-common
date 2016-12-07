3156d4dca0bfb5224c8e4be72108c1b9c6428f5b
Topbit Du <topbit.du@gmail.com>
2016-12-07 13:10:39 +0800

    1, Improve the Secure Column concern for the un-selected secure columns.

--- app/models/unidom/common/concerns/secure_column.rb
+++ app/models/unidom/common/concerns/secure_column.rb
@@ -8,14 +8,43 @@ module Unidom::Common::Concerns::SecureColumn
 
     cattr_accessor :secure_columns
 
+    def do_encrypt_secure_column(name)
+      name    = name.to_s
+      content = { 'nonce' => SecureRandom.hex(8), 'timestamp' => Time.now.to_i }
+      secure_columns[name].each do |field| content[field.to_s] = send(field) end
+      content = content.sort.to_h.to_json
+      aes_key = Digest::SHA512::digest self.class.exact_signature(self.class, name, '')
+      encoded = hex_encrypt content, key: aes_key
+      json = {
+          encoded:   encoded,
+          signature: Unidom::Common::Numeration.hex(self.class.exact_signature self.class, name, content),
+          encryption_algorithm: self.class.encryption_algorithm
+        }
+      send "#{name}=", json
+    end
+
+    def do_decrypt_secure_column(name)
+      name = name.to_sym
+      return unless respond_to? name
+      json = send(name)
+      return if json['encoded'].blank?||json['signature'].blank?||json['encryption_algorithm'].blank?
+      return if self.class.encryption_algorithm!=json['encryption_algorithm']
+      aes_key = Digest::SHA512::digest self.class.exact_signature(self.class, name, '')
+      content = decrypt Unidom::Common::Numeration.rev_hex(json['encoded']), key: aes_key
+      actual_signature = self.class.exact_signature(self.class, name, content)
+      return if Unidom::Common::Numeration.rev_hex(json['signature'])!=actual_signature
+      parsed = JSON.parse content
+      parsed.each do |key, value| send "#{key}=", value unless [ 'nonce', 'timestamp' ].include? key end
+    end
+
   end
 
   module ClassMethods
 
     def secure_column(name, fields: [])
 
-      name           = name.to_s
-      secure_columns = secure_columns||{}
+      name                = name.to_s
+      self.secure_columns = self.secure_columns||{}
       if secure_columns[name].present?
         raise ArgumentError.new("The #{name} column was defined as a secure column already.")
       else
@@ -24,33 +53,8 @@ module Unidom::Common::Concerns::SecureColumn
       fields.each do |field| attr_accessor field.to_sym if columns_hash[field.to_s].blank? end
 
       instance_eval do
-
-        before_save do
-          content = { 'nonce' => SecureRandom.hex(8), 'timestamp' => Time.now.to_i }
-          secure_columns[name].each do |field| content[field.to_s] = send(field) end
-          content = content.sort.to_h.to_json
-          aes_key = Digest::SHA512::digest self.class.exact_signature(self.class, name, '')
-          encoded = hex_encrypt content, key: aes_key
-          json = {
-              encoded:   encoded,
-              signature: Unidom::Common::Numeration.hex(self.class.exact_signature self.class, name, content),
-              encryption_algorithm: self.class.encryption_algorithm
-            }
-          send "#{name}=", json
-        end
-
-        after_find do
-          json = send(name)
-          return if json['encoded'].blank?||json['signature'].blank?||json['encryption_algorithm'].blank?
-          return if self.class.encryption_algorithm!=json['encryption_algorithm']
-          aes_key = Digest::SHA512::digest self.class.exact_signature(self.class, name, '')
-          content = decrypt Unidom::Common::Numeration.rev_hex(json['encoded']), key: aes_key
-          actual_signature = self.class.exact_signature(self.class, name, content)
-          return if Unidom::Common::Numeration.rev_hex(json['signature'])!=actual_signature
-          parsed = JSON.parse content
-          parsed.each do |key, value| send "#{key}=", value unless [ 'nonce', 'timestamp' ].include? key end
-        end
-
+        before_save do do_encrypt_secure_column name        end
+        after_find  do do_decrypt_secure_column name.to_sym end
       end
 
     end
