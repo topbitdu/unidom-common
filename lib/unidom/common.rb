c45f89402487f957b1838ffc2234f75a2cfdb74d
Topbit Du <topbit.du@gmail.com>
2017-01-17 11:55:08 +0800

    1, Improve the Common module to add the .configure method.

--- lib/unidom/common.rb
+++ lib/unidom/common.rb
@@ -16,5 +16,25 @@ module Unidom
     #FROM_DATE = '1970-01-01'.freeze
     #THRU_DATE = '3000-01-01'.freeze
 
+    mattr_accessor :options
+
+    def self.configure
+
+      options = {}
+      yield options
+
+      default_options = {
+        neglected_namespaces: []
+      }
+      self.options = default_options.merge options
+
+      puts 'Unidom::Common:'
+      if self.options[:neglected_namespaces].present?
+        puts '-- neglected_namespaces'
+        puts "   -> #{self.options[:neglected_namespaces].join ', '}"
+      end
+
+    end
+
   end
 end
