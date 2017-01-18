f7eee4b4181c752921f4de3a612bda559f2163b9
Topbit Du <topbit.du@gmail.com>
2017-01-18 11:57:29 +0800

    1, Improve the Model Extension concern to integrate the Neglection class.

--- lib/unidom/common/engine_extension.rb
+++ lib/unidom/common/engine_extension.rb
@@ -22,7 +22,7 @@ module Unidom::Common::EngineExtension
 
       if migration_enabled
         initializer :append_migrations do |app|
-          config.paths['db/migrate'].expanded.each { |expanded_path| app.config.paths['db/migrate'] << expanded_path } unless app.root.to_s.match root.to_s
+          config.paths['db/migrate'].expanded.each { |expanded_path| app.config.paths['db/migrate'] << expanded_path } unless Unidom::Common::Neglection.namespace_neglected?(self.class.name)||app.root.to_s.match(root.to_s)
         end
       end
 
