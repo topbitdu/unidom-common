2e6868d8e3127ffa826b760ded8eac4364017f93
Topbit Du <topbit.du@gmail.com>
2017-01-24 09:30:39 +0800

    1, Improve the Neglection class in case of lack of configuration.

--- lib/unidom/common/neglection.rb
+++ lib/unidom/common/neglection.rb
@@ -1,7 +1,8 @@
 class Unidom::Common::Neglection
 
   def self.namespace_neglected?(class_name)
-    Unidom::Common.options[:neglected_namespaces].include? class_name.to_s.deconstantize
+    neglected_namespaces = Unidom::Common.options[:neglected_namespaces]
+    neglected_namespaces.present? ? neglected_namespaces.include?(class_name.to_s.deconstantize) : false
   end
 
 end
