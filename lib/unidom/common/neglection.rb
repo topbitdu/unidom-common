bca21367b1736636de56354af76a976326bfeffe
Topbit Du <topbit.du@gmail.com>
2017-01-29 10:30:09 +0800

    1, Improve the Neglection class for the document.

--- lib/unidom/common/neglection.rb
+++ lib/unidom/common/neglection.rb
@@ -3,6 +3,16 @@
 
 class Unidom::Common::Neglection
 
+  ##
+  # 判断指定的类名是否被忽略。如:
+  # Unidom::Common::Neglection.namespace_neglected? 'Namespace::ClassName'
+  # 在应用的 config/initializers/unidom.rb 文件中配置如下代码，即可忽略 Namespace::ClassName 这个类。
+  # Unidom::Common.configure do |options|
+  # # The migrations inside the following namespaces will be ignored. The models inside the following namespaces won't be defined.
+  # options[:neglected_namespaces] = %w{
+  #   Namespace::ClassName
+  # }
+
   def self.namespace_neglected?(class_name)
     neglected_namespaces = Unidom::Common.options[:neglected_namespaces]
     neglected_namespaces.present? ? neglected_namespaces.include?(class_name.to_s.deconstantize) : false
