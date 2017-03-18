673af35fa061d5a36b1e5cc8a8fe085a870851c3
Topbit Du <topbit.du@gmail.com>
2017-03-18 15:57:43 +0800

    1, Improve the SHA-2 Digester for the document.

--- app/models/unidom/common/concerns/sha2_digester.rb
+++ app/models/unidom/common/concerns/sha2_digester.rb
@@ -21,6 +21,16 @@ module Unidom::Common::Concerns::Sha2Digester
       self.class.digest message, pepper: pepper
     end
 
+    ##
+    # 对明文 message 进行 SHA-2 摘要，并以16进制的形式返回， pepper 是用于增加混乱的内容。如：
+    # class SomeModel
+    #   include Unidom::Common::Concerns::Sha2Digester
+    #   def some_method(param_1)
+    #     hex_digest param_1
+    #     # 或者
+    #     hex_digest param_1, pepper: 'my_pepper'
+    #   end
+    # end
     def hex_digest(message, pepper: nil)
       self.class.hex_digest message, pepper: pepper
     end
