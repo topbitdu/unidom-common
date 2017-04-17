fedb304da0c7fba5519416242678d104c3239e3a
Topbit Du <topbit.du@gmail.com>
2017-04-17 13:47:04 +0800

    1, Improve the Argument Validation concern for the document.

--- app/models/unidom/common/concerns/argument_validation.rb
+++ app/models/unidom/common/concerns/argument_validation.rb
@@ -18,6 +18,9 @@ module Unidom::Common::Concerns::ArgumentValidation
 
   module ClassMethods
 
+    ##
+    # 断言给定的参数 value 非空。如果为空，则抛出 ArgumentError 异常。如：
+    # assert_present! :person, person
     def assert_present!(name, value)
       raise ArgumentError.new("The #{name} argument is required.") if value.blank?
     end
